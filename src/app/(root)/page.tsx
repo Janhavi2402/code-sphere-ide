7433490311a3a377a0421009e881444097375fb8
Janhavi Patil <jpatil2670@gmail.com>
2024-12-22 12:19:58 +0530

    editor panel done

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,5 @@
 
+import EditorPanel from "./_components/EditorPanel";
 import Header from "./_components/Header";
 
 
@@ -7,7 +8,7 @@ export default function Home() {
     <div className="min-h-screen">
       <div className="max-w-[1800px] mx-auto p-4">
         <Header />
-
+<EditorPanel/>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
          
          
